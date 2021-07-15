d50a8a74e5477e36c4b88864753638cd0f4551e4
jitp <georgies1me@gmail.com>
2021-07-15 20:27:43 -0300

    Refactor main app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { BoardModule } from '@app/modules';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    BoardModule,
+    SharedModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
