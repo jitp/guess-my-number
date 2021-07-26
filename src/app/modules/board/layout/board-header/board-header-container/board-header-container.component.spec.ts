71a35b8ad68ba0cd50a7a2eaf5bb6ab0a5c508d5
jitp <georgies1me@gmail.com>
2021-07-26 16:16:39 -0300

    Test board header component

--- src/app/modules/board/layout/board-header/board-header-container/board-header-container.component.spec.ts
+++ src/app/modules/board/layout/board-header/board-header-container/board-header-container.component.spec.ts
@@ -1,25 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BoardSandbox } from '@app/modules/board/services';
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator';
+import { cold, getTestScheduler } from 'jasmine-marbles';
+import { MockComponents } from 'ng-mocks';
+import { BasePage, createPage } from 'src/tests/helpers';
+import { BoardHeaderUiComponent } from '../board-header-ui/board-header-ui.component';
 import { BoardHeaderContainerComponent } from './board-header-container.component';
 
+type CSpectator = Spectator<BoardHeaderContainerComponent>;
+
+class Page extends BasePage<CSpectator> {
+  get uiCmp(): BoardHeaderUiComponent | null {
+    return this.queryComponent(BoardHeaderUiComponent);
+  }
+
+  clickAgain(): this {
+    this.uiCmp?.again.emit();
+
+    this.spectator.detectChanges();
+
+    return this;
+  }
+}
+
 describe('BoardHeaderContainerComponent', () => {
-  let component: BoardHeaderContainerComponent;
-  let fixture: ComponentFixture<BoardHeaderContainerComponent>;
+  let spectator: CSpectator;
+  let page: Page;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardHeaderContainerComponent ]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: BoardHeaderContainerComponent,
+    mocks: [BoardSandbox],
+    declarations: [MockComponents(BoardHeaderUiComponent)],
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardHeaderContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('creates the component', () => {
+    expect(createComponent().component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('passes the secret value', () => {
+    spectator = createComponent({
+      providers: [
+        mockProvider(BoardSandbox, { secretDisplay$: cold('x|', { x: 2 }) }),
+      ],
+    });
+    page = createPage(spectator, Page);
+
+    getTestScheduler().flush();
+
+    spectator.detectChanges();
+
+    expect(page.uiCmp?.secret).toBe(2);
+  });
+
+  it('starts over', () => {
+    spectator = createComponent();
+    page = createPage(spectator, Page);
+
+    page.clickAgain();
+
+    expect(spectator.inject(BoardSandbox).startOver).toHaveBeenCalled();
   });
 });
--- src/app/modules/board/layout/board-header/board-header-ui/board-header-ui.component.spec.ts
+++ src/app/modules/board/layout/board-header/board-header-ui/board-header-ui.component.spec.ts
@@ -1,25 +1,101 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { byTestId, createHostFactory, SpectatorHost } from '@ngneat/spectator';
+import { BasePage, createPage } from 'src/tests/helpers';
 import { BoardHeaderUiComponent } from './board-header-ui.component';
 
+type CSpectator = SpectatorHost<BoardHeaderUiComponent>;
+
+class Page extends BasePage<CSpectator> {
+  readonly TEST_IDS = {
+    AGAIN_BTN: 'play-again-btn',
+    BETWEEN_MSG: 'between-msg',
+    HEADER_TITLE: 'header-title',
+    GAME_SECRET: 'game-secret',
+  };
+
+  get playAgainBtn(): HTMLButtonElement | null {
+    return this.queryTestId(this.TEST_IDS.AGAIN_BTN);
+  }
+
+  get betweenMsg(): HTMLSpanElement | null {
+    return this.queryTestId(this.TEST_IDS.BETWEEN_MSG);
+  }
+
+  get headerTitle(): HTMLHeadingElement | null {
+    return this.queryTestId(this.TEST_IDS.HEADER_TITLE);
+  }
+
+  get gameSecret(): HTMLDivElement | null {
+    return this.queryTestId(this.TEST_IDS.GAME_SECRET);
+  }
+
+  clickAgainBtn(): this {
+    this.spectator.click(byTestId(this.TEST_IDS.AGAIN_BTN));
+
+    return this;
+  }
+}
+
 describe('BoardHeaderUiComponent', () => {
-  let component: BoardHeaderUiComponent;
-  let fixture: ComponentFixture<BoardHeaderUiComponent>;
+  let spectator: CSpectator;
+  let page: Page;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardHeaderUiComponent ]
-    })
-    .compileComponents();
+  const createHost = createHostFactory({
+    component: BoardHeaderUiComponent,
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardHeaderUiComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('creates the component', () => {
+    spectator = createHost(`<app-board-header-ui></app-board-header-ui>`);
+    expect(spectator.component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('renders secret with question mark', () => {
+    spectator = createHost(`<app-board-header-ui></app-board-header-ui>`);
+    page = createPage(spectator, Page);
+
+    expect(page.gameSecret).toHaveText('?');
+  });
+
+  it('renders secret number', () => {
+    spectator = createHost(
+      `<app-board-header-ui [secret]="secret"></app-board-header-ui>`,
+      {
+        hostProps: {
+          secret: 4,
+        },
+      }
+    );
+    page = createPage(spectator, Page);
+
+    expect(page.gameSecret).toHaveText('4');
+  });
+
+  it("renders the 'between message'", () => {
+    spectator = createHost(`<app-board-header-ui></app-board-header-ui>`);
+    page = createPage(spectator, Page);
+
+    expect(page.betweenMsg).toHaveText('(Between 1 and 20)');
+  });
+
+  it("renders the 'header title'", () => {
+    spectator = createHost(`<app-board-header-ui></app-board-header-ui>`);
+    page = createPage(spectator, Page);
+
+    expect(page.headerTitle).toHaveText('Guess My Number!');
+  });
+
+  it('emits again event when clicking on again btn', () => {
+    spectator = createHost(
+      `<app-board-header-ui (again)="handleAgain()"></app-board-header-ui>`,
+      {
+        hostProps: {
+          handleAgain: jasmine.createSpy('handleAgain').and.callThrough(),
+        },
+      }
+    );
+    page = createPage(spectator, Page);
+
+    page.clickAgainBtn();
+
+    expect((spectator.hostComponent as any).handleAgain).toHaveBeenCalled();
   });
 });
