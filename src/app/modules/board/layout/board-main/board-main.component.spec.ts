506096627acdea98720eb2bbb40c186eea185c9c
jitp <georgies1me@gmail.com>
2021-07-26 17:33:06 -0300

    Test board main component

--- src/app/modules/board/layout/board-main/board-main.component.spec.ts
+++ src/app/modules/board/layout/board-main/board-main.component.spec.ts
@@ -1,25 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { MockComponents } from 'ng-mocks';
+import { BasePage, createPage } from 'src/tests/helpers';
+import {
+  HighScoreContainerComponent,
+  InputContainerComponent,
+  MessageContainerComponent,
+  ScoreContainerComponent,
+} from '../../components';
 import { BoardMainComponent } from './board-main.component';
 
+type CSpectator = Spectator<BoardMainComponent>;
+
+class Page extends BasePage<CSpectator> {
+  get inputCmp(): InputContainerComponent | null {
+    return this.queryComponent(InputContainerComponent);
+  }
+
+  get msgCmp(): MessageContainerComponent | null {
+    return this.queryComponent(MessageContainerComponent);
+  }
+
+  get scoreCmp(): ScoreContainerComponent | null {
+    return this.queryComponent(ScoreContainerComponent);
+  }
+
+  get highScoreCmp(): HighScoreContainerComponent | null {
+    return this.queryComponent(HighScoreContainerComponent);
+  }
+}
+
 describe('BoardMainComponent', () => {
-  let component: BoardMainComponent;
-  let fixture: ComponentFixture<BoardMainComponent>;
+  let spectator: CSpectator;
+  let page: Page;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardMainComponent ]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: BoardMainComponent,
+    declarations: [
+      MockComponents(
+        InputContainerComponent,
+        MessageContainerComponent,
+        ScoreContainerComponent,
+        HighScoreContainerComponent
+      ),
+    ],
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardMainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('creates the component', () => {
+    expect(createComponent().component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('renders the input container', () => {
+    spectator = createComponent();
+    page = createPage(spectator, Page);
+
+    expect(page.inputCmp).toBeTruthy();
+  });
+
+  it('renders the message container', () => {
+    spectator = createComponent();
+    page = createPage(spectator, Page);
+
+    expect(page.msgCmp).toBeTruthy();
+  });
+
+  it('renders the score container', () => {
+    spectator = createComponent();
+    page = createPage(spectator, Page);
+
+    expect(page.scoreCmp).toBeTruthy();
+  });
+
+  it('renders the high score container', () => {
+    spectator = createComponent();
+    page = createPage(spectator, Page);
+
+    expect(page.highScoreCmp).toBeTruthy();
   });
 });
